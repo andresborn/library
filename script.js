82cebcc0bd0fe76f93865e1079ef19d4124c0118
andresborn <andres.born@gmail.com>
2020-12-18 14:49:46 -0600

    Added btns to book object in html, css. Rearr js.

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-//let myLibrary = [theHobbit];
+// Declare book objects two ways. Dummy tests
+let theLordOfTheRings = {
+    title : "The Lord of the Rings",
+    author : "J.R.R. Tolkien",
+    pages : "1000",
+    read : false,
+};
+let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', true);
 
+let myLibrary = [theHobbit, theLordOfTheRings];
+
+// Book constructor
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
 }
-
 Book.prototype.info = function() {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
 };
 
-
-let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'true');
-
+// DOM methods
 const formContainer = document.querySelector('#form-container');
+
+// Open/close form
 const openForm = document.querySelector('#open-form');
 openForm.addEventListener('click', () => {
     formContainer.setAttribute('style', 'display: flex;');
@@ -23,3 +32,7 @@ const closeForm = document.querySelector('#close-form');
 closeForm.addEventListener('click', () => {
     formContainer.setAttribute('style', 'display: none;');
 })
+
+function takeUserInput() {
+
+}
